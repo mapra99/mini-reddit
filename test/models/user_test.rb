cab6eadcbcee26e08af233e625e0e9b745237ec1
Miguel Prada <mapra99@gmail.com>
2019-10-09 17:07:02 -0500

    Added tests for User Name Validations

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,26 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = User.new(name: "Example User", email: "user@example.com")
+  end
+
+  test "should be valid" do
+    assert @user.valid?
+  end
+
+  test "name is too short" do
+    @user.name = "aaa"
+    assert_not @user.valid?
+  end
+
+  test "name is too long" do
+    @user.name = "a"*141
+    assert_not @user.valid?
+  end
+
+  test "name is empty" do
+    @user.name = nil
+    assert_not @user.valid?
+  end
 end
