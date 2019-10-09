3c6ef0018e10b73afdf6370be8775b45ca3381a9
Miguel Prada <mapra99@gmail.com>
2019-10-09 12:16:50 -0500

    added foreign key to posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,6 @@
 class Post < ApplicationRecord
+    belongs_to :users
+
     validates :title, presence: true, length: {in: 3..50}, uniqueness: true
     validates :content, presence: true, length: {in: 50..1000}
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_09_165536) do
+ActiveRecord::Schema.define(version: 2019_10_09_170912) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.string "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
