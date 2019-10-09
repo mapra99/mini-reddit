38dbfad675bc36ab953f2c30e9cb4f74227380b9
Miguel Prada <mapra99@gmail.com>
2019-10-09 12:56:01 -0500

    fixed association problem

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
-    belongs_to :users
+    belongs_to :user
 
     validates :title, presence: true, length: {in: 3..50}, uniqueness: true
     validates :content, presence: true, length: {in: 50..1000}
