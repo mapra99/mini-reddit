b139835004a3246ddfd7d12e5837a26d1ccd4040
dev1980 <brham1980@gmail.com>
2019-10-09 22:08:37 +0545

    user validation added

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+    validates :name, length: {maximum: 140, minimum: 4}, presence: true,
+    format: { with: /\A[a-zA-Z ]+\z/}
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX }
+
+end
