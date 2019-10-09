9e7bded382dd16e97f3caa508976a357fbe5f151
dev1980 <brham1980@gmail.com>
2019-10-10 04:40:44 +0545

    validation for comment is completed

--- test/models/comment_test.rb
+++ test/models/comment_test.rb
@@ -4,4 +4,29 @@ class CommentTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  def setup
+    @user = User.new(name: "Example User", email: "user@example.com")
+    @user.save
+    @post = User.first.posts.build
+    @post.title = "This is fist post"
+    @post.content = "a"* 60
+    @post.save
+    @comment = Comment.new(user_id:1, post_id:1, body: "a"*100)
+  end
+
+  test "commnet is too short" do
+    @comment.body = "aaa"
+    assert_not @comment.valid?
+  end
+
+  test "comment is too long" do
+    @comment.body = "a"*201
+    assert_not @comment.valid?
+  end
+
+  test "comment is empty" do
+    @comment.body = nil
+    assert_not @comment.valid?
+  end
 end
