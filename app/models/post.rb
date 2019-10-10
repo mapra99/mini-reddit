4e818f404197c13d2254758a88cf05ff258f9677
Miguel Prada <mapra99@gmail.com>
2019-10-10 10:38:01 -0500

    added dependent: destroy parameter to has_many associations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
     belongs_to :user
-    has_many :comments
+    has_many :comments, dependend: :destroy
     validates :title, presence: true, length: {in: 3..50}, uniqueness: true
     validates :content, presence: true, length: {in: 50..1000}
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
-    has_many :posts
-    has_many :comments
+    has_many :posts, dependent: :destroy
+    has_many :comments, dependent: :destroy
+    
     validates :name, length: {maximum: 140, minimum: 4}, presence: true,
     format: { with: /\A[a-zA-Z ]+\z/}
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
