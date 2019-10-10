ed8596cb5ef37b4fc2e9cdecfe8dda9738e7c35d
Miguel Prada <mapra99@gmail.com>
2019-10-10 10:51:49 -0500

    lint errors fixed on comment.rb

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 class Comment < ApplicationRecord
-    belongs_to :post
-    belongs_to :user
-    validates :body, presence: true, length: {in: 10..200 }
+  belongs_to :post
+  belongs_to :user
+  validates :body, presence: true, length: { in: 10..200 }
 end
