962ccd2cd918b10e09019901aefdc9a2b6d945c1
Miguel Prada <mapra99@gmail.com>
2019-10-10 10:50:14 -0500

    lint errors fixed on user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,13 @@
+# frozen_string_literal: true
+
 class User < ApplicationRecord
-    has_many :posts, dependent: :destroy
-    has_many :comments, dependent: :destroy
-    
-    validates :name, length: {maximum: 140, minimum: 4}, presence: true,
-    format: { with: /\A[a-zA-Z ]+\z/}
-    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-    validates :email, presence: true, length: { maximum: 255 },
+  has_many :posts, dependent: :destroy
+  has_many :comments, dependent: :destroy
+
+  validates :name, length: { maximum: 140, minimum: 4 }, presence: true,
+                   format: { with: /\A[a-zA-Z ]+\z/ }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
+  validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: true
 end
