665c9eb591f294452fa5010caebdcba92df17643
Miguel Prada <mapra99@gmail.com>
2019-10-10 10:51:03 -0500

    lint errors fixed on post.rb

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,8 @@
+# frozen_string_literal: true
+
 class Post < ApplicationRecord
-    belongs_to :user
-    has_many :comments, dependend: :destroy
-    validates :title, presence: true, length: {in: 3..50}, uniqueness: true
-    validates :content, presence: true, length: {in: 50..1000}
+  belongs_to :user
+  has_many :comments, dependend: :destroy
+  validates :title, presence: true, length: { in: 3..50 }, uniqueness: true
+  validates :content, presence: true, length: { in: 50..1000 }
 end
