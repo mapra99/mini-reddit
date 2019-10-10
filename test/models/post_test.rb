0a4f421e6a588b70ab29d38eb202c0e80dc34e7e
Miguel Prada <mapra99@gmail.com>
2019-10-10 10:57:01 -0500

    lint errors fixed on post_test.rb

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,54 +1,53 @@
+# frozen_string_literal: true
+
 require 'test_helper'
 
 class PostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
   def setup
-    @user = User.new(name: "Example User", email: "user@example.com")
+    @user = User.new(name: 'Example User', email: 'user@example.com')
     @user.save
     @post = User.first.posts.build
-    @post.title = "This is fist post"
-    @post.content = "a"* 60
+    @post.title = 'This is fist post'
+    @post.content = 'a' * 60
   end
-  
-  test "should be valid" do
+
+  test 'should be valid' do
     assert @post.valid?
   end
 
-  test "test if title is empty" do
+  test 'test if title is empty' do
     @post.title = nil
     assert_not @post.valid?
   end
 
-  test "title too short" do
-    @post.title = "aa"
+  test 'title too short' do
+    @post.title = 'aa'
     assert_not @post.valid?
   end
 
-  test "title too long" do
-    @post.title = "a"*62
+  test 'title too long' do
+    @post.title = 'a' * 62
     assert_not @post.valid?
   end
 
-  test "title must be unique" do
+  test 'title must be unique' do
     @post.save
     post2 = @post.dup
     assert_not post2.valid?
   end
 
-  test "test if content is empty" do
+  test 'test if content is empty' do
     @post.content = nil
     assert_not @post.valid?
   end
 
-  test "content too short" do
-    @post.content = "aaaa"
+  test 'content too short' do
+    @post.content = 'aaaa'
     assert_not @post.valid?
   end
 
-  test "content too long" do
-    @post.content = "a"*1001
+  test 'content too long' do
+    @post.content = 'a' * 1001
     assert_not @post.valid?
   end
 end
