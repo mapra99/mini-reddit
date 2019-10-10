fe96e8224af782f22bff15e0696c555b1da29811
dev1980 <brham1980@gmail.com>
2019-10-10 23:59:15 +0545

    Syntax error fixed

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,7 @@
 
 class Post < ApplicationRecord
   belongs_to :user
-  has_many :comments, dependend: :destroy
+  has_many :comments, dependent: :destroy
   validates :title, presence: true, length: { in: 3..50 }, uniqueness: true
   validates :content, presence: true, length: { in: 50..1000 }
 end
